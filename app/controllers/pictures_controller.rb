25c47df295ac5f39fd966221e4f27051d3a51656
fumischo <nil.admirari1009@gmail.com>
2019-10-14 23:47:09 +0900

    アソシエーション追記

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,74 +1,73 @@
 class PicturesController < ApplicationController
   before_action :set_picture, only: [:show, :edit, :update, :destroy]
 
-  # GET /pictures
-  # GET /pictures.json
   def index
-    @pictures = Picture.all
+    if current_user.present?
+      @pictures = Picture.all
+    else
+      flash[:notice] = "ログインしてください！"
+      redirect_to new_session_path
+    end
   end
 
-  # GET /pictures/1
-  # GET /pictures/1.json
-  def show
-  end
-
-  # GET /pictures/new
   def new
-    @picture = Picture.new
+    if params[:back]
+      @picture = Picture.new(picture_params)
+    else
+      @picture = Picture.new
+    end
   end
 
-  # GET /pictures/1/edit
-  def edit
-  end
-
-  # POST /pictures
-  # POST /pictures.json
   def create
-    @picture = Picture.new(picture_params)
-
-    respond_to do |format|
-      if @picture.save
-        format.html { redirect_to @picture, notice: 'Picture was successfully created.' }
-        format.json { render :show, status: :created, location: @picture }
-      else
-        format.html { render :new }
-        format.json { render json: @picture.errors, status: :unprocessable_entity }
-      end
+    @picture = current_user.pictures.build(picture_params)
+    if @picture.save
+      # ContactMailer.contact_mail(@picture).deliver
+      redirect_to pictures_path, notice: "記事を投稿しました！"
+    else
+      render 'index'
     end
   end
 
-  # PATCH/PUT /pictures/1
-  # PATCH/PUT /pictures/1.json
+  def show
+    @favorite = current_user.favorites.find_by(picture_id: @picture.id)
+  end
+
+  def edit
+    if @picture.user_id == session[:user_id]
+      else
+        redirect_to pictures_path, notice: "他のユーザーの記事は編集できません！"
+    end
+  end
+
   def update
-    respond_to do |format|
-      if @picture.update(picture_params)
-        format.html { redirect_to @picture, notice: 'Picture was successfully updated.' }
-        format.json { render :show, status: :ok, location: @picture }
-      else
-        format.html { render :edit }
-        format.json { render json: @picture.errors, status: :unprocessable_entity }
-      end
+    if @picture.update(picture_params)
+      redirect_to pictures_path, notice: "ブログを編集しました！"
+    else
+      render 'edit'
     end
   end
 
-  # DELETE /pictures/1
-  # DELETE /pictures/1.json
+  def confirm
+    @picture = current_user.pictures.build(picture_params)
+    render :new if @picture.invalid?
+  end
+
   def destroy
-    @picture.destroy
-    respond_to do |format|
-      format.html { redirect_to pictures_url, notice: 'Picture was successfully destroyed.' }
-      format.json { head :no_content }
+    if @picture.user_id == session[:user_id]
+      @picture.destroy
+      redirect_to pictures_path, notice:"記事を削除しました！ "
+      else
+        redirect_to pictures_path, notice: "他のユーザーの記事は削除できません！"
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_picture
-      @picture = Picture.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def picture_params
-      params.require(:picture).permit(:image, :content, :user_id)
-    end
+  def picture_params
+    params.require(:picture).permit(:image, :image_cache, :content)
+  end
+
+  def set_picture
+    @picture = Picture.find(params[:id])
+  end
 end
