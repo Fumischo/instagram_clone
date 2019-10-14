b9749a0820d909ef6dce0b241ffc91972643934f
fumischo <nil.admirari1009@gmail.com>
2019-10-15 02:48:20 +0900

    altered some things

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,7 @@
 class Picture < ApplicationRecord
+  validates :content, presence: true, length: { maximum: 140 }
+  validates :image, presence: true
   belongs_to :user
+  mount_uploader :image, ImageUploader
+  has_many :favorites, dependent: :destroy
 end
