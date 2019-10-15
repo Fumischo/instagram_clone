429a80ab83821df196ecc5ca1bc23a56c94fb59d
fumischo <nil.admirari1009@gmail.com>
2019-10-15 16:05:22 +0900

    お気に入りのpictureの一覧画面作成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -8,4 +8,8 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
     redirect_to pictures_url, notice: "#{favorite.picture.user.name}さんのブログをお気に入り解除しました"
   end
+
+  def show
+    @favorite = current_user.favorite_pictures
+  end
 end
