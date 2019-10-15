54450e47f9e1eb8b73bc86aa05432a4a6ea3f52b
fumischo <nil.admirari1009@gmail.com>
2019-10-15 19:09:01 +0900

    バリデーションエラーメッセージなどの日本語化

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   root to: 'pictures#index'
   resources :users, only: [:new, :create, :show, :destroy ]
   resources :sessions, only: [:new, :create, :destroy]
+  resources :favorites, only: [:create, :destroy,:show]
   resources :pictures do
     collection do
       post :confirm
     end
   end
-  resources :favorites, only: [:create, :destroy,:show]
 
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
