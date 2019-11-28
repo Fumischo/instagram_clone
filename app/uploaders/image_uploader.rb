bc4c72c1a7e7a19c4493d0bc90420b9d2c849e71
fumischo <nil.admirari1009@gmail.com>
2019-11-28 14:48:43 +0900

    modify some codes

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,7 +8,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   if Rails.env.production?
     storage :fog
   elsif Rails.env.development? 
-    storage :file
+    storage :fog
   end
   # storage :fog
   process :resize_to_limit => [400, 400]
